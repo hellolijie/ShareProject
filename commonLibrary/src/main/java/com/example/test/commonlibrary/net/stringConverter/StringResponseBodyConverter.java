7f47750c53d08705ed33bc653b9af3adc770619e
jerry <353733664@qq.com>
2017-04-10 16:42:08 +0800

    StringConverter

---
+++ commonLibrary/src/main/java/com/example/test/commonlibrary/net/stringConverter/StringResponseBodyConverter.java
@@ -0,0 +1,24 @@
+package com.example.test.commonlibrary.net.stringConverter;
+
+/**
+ * Created by lijie on 2017/4/10.
+ */
+
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import retrofit2.Converter;
+
+/**
+ * Created by 耿 on 2016/9/6.
+ */
+public class StringResponseBodyConverter implements Converter<ResponseBody, String> {
+    @Override
+    public String convert(ResponseBody value) throws IOException {
+        try {
+            return value.string();
+        } finally {
+            value.close();
+        }
+    }
+}
