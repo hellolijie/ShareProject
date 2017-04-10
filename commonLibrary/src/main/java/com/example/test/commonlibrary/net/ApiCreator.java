ddb4e05ce913bbaf17ff853b3770d0387a369833
jerry <353733664@qq.com>
2017-04-10 16:43:53 +0800

    stringConverter

--- commonLibrary/src/main/java/com/example/test/commonlibrary/net/ApiCreator.java
+++ commonLibrary/src/main/java/com/example/test/commonlibrary/net/ApiCreator.java
@@ -1,11 +1,21 @@
 package com.example.test.commonlibrary.net;
 
+import com.example.test.commonlibrary.net.stringConverter.StringConverterFactory;
+
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+
 import okhttp3.OkHttpClient;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
 import retrofit2.CallAdapter;
 import retrofit2.Converter;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
+
+import static android.R.attr.value;
 
 /**
  * Created by lijie on 2017/3/28.
@@ -21,7 +31,8 @@ public class ApiCreator {
         Retrofit retrofit = new Retrofit.Builder()
                 .client(okHttpClient)
                 .baseUrl(baseURl)
-                .addConverterFactory(gsonConverterFactory)
+//                .addConverterFactory(gsonConverterFactory)
+                .addConverterFactory(StringConverterFactory.create())
                 .addCallAdapterFactory(rxJavaCallAdapterFactory)
                 .build();
 
