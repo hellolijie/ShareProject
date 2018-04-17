5d9dab1ab1d033f35ddb6bcbc0939a45dbcfebc9
jerry <353733664@qq.com>
2018-04-17 14:22:03 +0800

    最后提交

---
+++ simpleNettylibrary/src/main/java/cn/huna/jerry/simplenettylibrary/request/RequestCallback.java
@@ -0,0 +1,16 @@
+package cn.huna.jerry.simplenettylibrary.request;
+
+
+import cn.huna.jerry.simplenettylibrary.model.ErrorModel;
+
+/**
+ * 请求回调
+ */
+public abstract class RequestCallback {
+    public long requestCreateTime;      //请求创建时间
+    public long timeOverMilliseconds;    //超时时间
+    public String requestKey;           //请求唯一标识
+
+    public abstract void onSuc(String msgContent);
+    public abstract void onError(ErrorModel errorModel);
+}
